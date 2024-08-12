aaac490f4fbc817e1db32fdc1ac8d205f26aa147
bartek <bartzagr@gmail.com>
2024-08-12 14:37:40 +0200

    chore: setup tooling

---
+++ tooling/tailwind/eslint.config.js
@@ -0,0 +1,6 @@
+// FIXME: This kinda stinks...
+/// <reference types="../../tooling/eslint/types.d.ts" />
+
+import baseConfig from "@turbostarter/eslint-config/base";
+
+export default [...baseConfig];
